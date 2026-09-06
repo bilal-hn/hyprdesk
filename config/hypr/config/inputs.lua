bfc2a3be4a197e0bbdf4ad81a60dea9ca311eb3a
bilal-hn <bilal.edu7215@gmail.com>
2026-09-06 13:30:28 +0500

    feat: tiles config and binding

---
+++ config/hypr/config/inputs.lua
@@ -0,0 +1,21 @@
+-- Input configuration
+
+hl.config({
+    input = {
+        -- sensitivity = -0.25,
+        accel_profile = "flat",
+        touchpad = {
+            natural_scroll = true,  -- Change to false if it is currently moving backwards
+            tap_to_click = true,
+        },
+    },
+    -- Uncomment the section below to enable software cursors; this can help with cursor display or behavior issues
+    -- cursor = {
+    --     no_hardware_cursors = 1,
+    -- },
+})
+
+hl.gesture({ fingers = 4, direction = "horizontal", action = "workspace" })
+hl.gesture({ fingers = 3, direction = "down",       action = "close" })
+hl.gesture({ fingers = 3, direction = "up",         action = "fullscreen" })
+hl.gesture({ fingers = 3, direction = "left",       action = "float" })
